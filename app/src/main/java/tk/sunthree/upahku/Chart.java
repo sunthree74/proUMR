2e98a1d580af5438ad0de58cc969bd618bd9cf59
sunthree74 <nurmuhlis74@yahoo.co.id>
2018-12-18 10:08:31 +0700

    first commit

---
+++ app/src/main/java/tk/sunthree/upahku/Chart.java
@@ -0,0 +1,64 @@
+package tk.sunthree.upahku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart);
+        setTitle("UMR Apps");
+
+        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+        anyChartView.setProgressBar(findViewById(R.id.progress_bar));
+
+        Cartesian cartesian = AnyChart.column();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("2016", 3100000));
+        data.add(new ValueDataEntry("2017", 3355750));
+        data.add(new ValueDataEntry("2018", 3648035));
+        data.add(new ValueDataEntry("2019", 3940973));
+
+        Column column = cartesian.column(data);
+
+        column.tooltip()
+                .titleFormat("{%X}")
+                .position(Position.CENTER_BOTTOM)
+                .anchor(Anchor.CENTER_BOTTOM)
+                .offsetX(0d)
+                .offsetY(5d)
+                .format("Rp. {%Value}{groupsSeparator: }");
+
+        cartesian.animation(true);
+        cartesian.title("Data UMP DKI Jakarta");
+
+        cartesian.yScale().minimum(0d);
+
+        cartesian.yAxis(0).labels().format("Rp. {%Value}{groupsSeparator: }");
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+
+        cartesian.xAxis(0).title("Tahun");
+        cartesian.yAxis(0).title("UMP");
+
+        anyChartView.setChart(cartesian);
+    }
+}
---
+++ app/src/main/java/tk/sunthree/upahku/MainActivity.java
@@ -0,0 +1,87 @@
+package tk.sunthree.upahku;
+
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    // List view
+    private ListView lv;
+    private Toolbar toolbar;
+    private EditText inputSearch;
+    private TextInputLayout inputLayoutSearch;
+
+    // Listview Adapter
+    ArrayAdapter<String> adapter;
+
+    // Search EditText
+//    EditText inputSearch;
+
+    // ArrayList for Listview
+    ArrayList<HashMap<String, String>> negaraList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("UMR Apps");
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        inputLayoutSearch = (TextInputLayout) findViewById(R.id.input_search_layout);
+        inputSearch = (EditText) findViewById(R.id.input_search);
+
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                MainActivity.this.adapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+//        // Data Array yang akan ditampilkan di list
+//        String kota[] = {"DKI Jakarta","Papua","Sulawesi Utara","Kepulauan Bangka Belitung",
+//                "Sulawesi Selatan" };
+//        int ump[] = {3100000,2435000,2400000,2314500,225000};
+//        // ambil list_view dan inputsearchnya di xml
+//        lv = (ListView) findViewById(R.id.listKota);
+//        inputSearch = (EditText) findViewById(R.id.edtSearch);
+//
+//        // proses menambahkan array kedalam listview
+//        adapter = new ArrayAdapter<String>(this, R.layout.item_list,
+//                R.id.txt1, kota);
+//        lv.setAdapter(adapter);
+//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+//                String nama_kota = lv.getItemAtPosition(position).toString();
+//                Intent intent = new Intent(getApplicationContext(),Chart.class);
+//
+//                startActivity(intent);
+//            }
+//        });
+    }
+}
